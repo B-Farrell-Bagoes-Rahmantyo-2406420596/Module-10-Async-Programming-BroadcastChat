29823c6770292f9afad15660bee3fcf056b1bded
Rekkin99 <farrellbagoes04@gmail.com>
2026-05-18 15:51:59 +0700

    Experiment 2.3: Small changes, add IP and Port

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -27,7 +27,8 @@ async fn handle_connection(
                     Some(Ok(msg)) => {
                         if let Some(text) = msg.as_text() {
                             println!("From client {addr:?} {text:?}");
-                            bcast_tx.send(text.into())?;
+                            let formatted = format!("[{}]: {}", addr, text);
+                            bcast_tx.send(formatted)?;
                         }
                     }
                     Some(Err(err)) => return Err(err.into()),
